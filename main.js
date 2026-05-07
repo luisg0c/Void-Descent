a41911b3f152f4cdc10a1cc89efc46230dea39cc
luisg0c <atkinsnetflix@gmail.com>
2026-05-07 15:40:00 -0300

    build(electron): windows portable .exe packaging via electron-builder

---
+++ main.js
@@ -0,0 +1,32 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+	const win = new BrowserWindow({
+		width: 1280,
+		height: 720,
+		minWidth: 640,
+		minHeight: 360,
+		title: 'Void Descent',
+		autoHideMenuBar: true,
+		backgroundColor: '#000000',
+		webPreferences: {
+			contextIsolation: true,
+			nodeIntegration: false
+		}
+	});
+
+	win.setMenuBarVisibility(false);
+	win.loadFile('index.html');
+}
+
+app.whenReady().then(() => {
+	createWindow();
+	app.on('activate', () => {
+		if (BrowserWindow.getAllWindows().length === 0) createWindow();
+	});
+});
+
+app.on('window-all-closed', () => {
+	if (process.platform !== 'darwin') app.quit();
+});
